f437e3f4931fb8dc4d39c579dd42ed48ceb26050
Yana <ba.ya.na@mail.ru>
2023-03-06 10:56:31 +0500

    minor fixes

--- Program.cs
+++ Program.cs
@@ -6,32 +6,17 @@ using System.Text;
 namespace Practice1 {
     public class Program { 
         static void Main(string[] args) {
-            String correctSymbols = "abcdefghijklmnopqrstuvwxyz";
             Console.Write("Введите строку: ");
             string str = Console.ReadLine();
             string result;
-            if (!str.Contains(@"[a-z]*"))
-            {
-                StringBuilder errorMessage = new StringBuilder("Ошибка, введены неверные символы: ");
-                List<char> errorSymbols = new List<char>();
-                for (int i = 0; i < str.Length; i++)
-                {
-                    if (!(correctSymbols.Contains(str[i])))
-                    {
-                        errorSymbols.Add(str[i]);
-                    }
-                }
-                for (int i = 0; i < errorSymbols.Count; i++)
-                {
-                    errorMessage.AppendFormat("{0}", errorSymbols[i]);
-                    if (i < errorSymbols.Count - 1)
-                    {
-                        errorMessage.Append(", ");
-                    }
-                }
-                Console.WriteLine(errorMessage);
+
+            var incorrectSymbols = GetIncorrectSymbols(str);
+            if (incorrectSymbols.Length != 0) {
+                incorrectSymbols.Remove(incorrectSymbols.Length - 2, 2);
+                Console.WriteLine($"Ошибка, введены неверные символы: {incorrectSymbols}.");
                 return;
             }
+
             if (str.Length % 2 == 0)
             {
                 char[] leftStr = str.Substring(0, str.Length / 2).ToCharArray();
@@ -46,6 +31,22 @@ namespace Practice1 {
                 result = new string(brokenStr) + str;
             }
             Console.WriteLine(result);
+        }
+
+        private static StringBuilder GetIncorrectSymbols(string inputString)
+        {
+            string correctSymbols = "abcdefghijklmnopqrstuvwxyz";
+            StringBuilder incorrectSymbols = new StringBuilder();
+
+            foreach (char ch in inputString)
+            {
+                if (!correctSymbols.Contains(ch))
+                {
+                    incorrectSymbols.AppendFormat("{0}, ", ch);
+                }
+            }
+            
+            return incorrectSymbols;
         }
     }
 }
