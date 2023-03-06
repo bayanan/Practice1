65df1ebf5831dc8458480b9d086ed5d04827b0a8
Yana <ba.ya.na@mail.ru>
2023-03-06 10:21:59 +0500

    Task 2 init

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Practice1 {
     public class Program { 
         static void Main(string[] args) {
+            String correctSymbols = "abcdefghijklmnopqrstuvwxyz";
             Console.Write("Введите строку: ");
             string str = Console.ReadLine();
             string result;
-            if (str.Length % 2 == 0) {
+            if (!str.Contains(@"[a-z]*"))
+            {
+                StringBuilder errorMessage = new StringBuilder("Ошибка, введены неверные символы: ");
+                List<char> errorSymbols = new List<char>();
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (!(correctSymbols.Contains(str[i])))
+                    {
+                        errorSymbols.Add(str[i]);
+                    }
+                }
+                for (int i = 0; i < errorSymbols.Count; i++)
+                {
+                    errorMessage.AppendFormat("{0}", errorSymbols[i]);
+                    if (i < errorSymbols.Count - 1)
+                    {
+                        errorMessage.Append(", ");
+                    }
+                }
+                Console.WriteLine(errorMessage);
+                return;
+            }
+            if (str.Length % 2 == 0)
+            {
                 char[] leftStr = str.Substring(0, str.Length / 2).ToCharArray();
                 char[] rightStr = str.Substring(str.Length / 2).ToCharArray();
                 Array.Reverse(leftStr);
